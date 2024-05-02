601d33649d22254adb3f412107ff99045bbb2760
adarshKhamitkar <42814496+adarshKhamitkar@users.noreply.github.com>
2024-05-02 21:53:52 +0530

    Update station_5_12.sql

--- SQL/station_5.sql
+++ SQL/station_5.sql
@@ -12,3 +12,18 @@ WHERE CITY NOT LIKE "A%" AND
              CITY NOT LIKE "%I" AND
              CITY NOT LIKE "%O" AND
              CITY NOT LIKE "%U";
+
+- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE CITY NOT LIKE "A%" AND
+             CITY NOT LIKE "E%" AND
+             CITY NOT LIKE "I%" AND
+             CITY NOT LIKE "O%" AND
+             CITY NOT LIKE "U%" AND
+             CITY NOT LIKE "%A" AND
+             CITY NOT LIKE "%E" AND
+             CITY NOT LIKE "%I" AND
+             CITY NOT LIKE "%O" AND
+             CITY NOT LIKE "%U";
